f37ff293f41dcc8c4be4c13460e77e0d2471321b
Chris Saldivar <chrisdsaldivar@gmail.com>
2020-03-12 19:11:39 -0500

    Implements A/V audio capture

--- public/js/stream.js
+++ public/js/stream.js
@@ -1,18 +1,63 @@
 'use strict';
 
 const ws = new WebSocket('ws://localhost:9090');
+let lastPeerId = null;
+let peer = null; // own peer object
+let conn = null;
+
+// Element refs
+const videoElem = document.getElementById("screen");
+const audioElem = document.getElementById("voice");
+
 
 ws.onopen = (event) => {
     console.log("WebSocket is open now.");
     ws.send('PING');
 };
-
 ws.onmessage = (event) => {
-    console.log("WebSocket message received:", event.data);
+    console.log(`WebSocket message received: ${event.data}`);
 }
 
 document.getElementById('shareScreen--button').onclick = shareScreen;
+document.getElementById('stopCapture--button').onclick = stopCapture;
 
 async function shareScreen () {
     console.log(navigator.mediaDevices.getSupportedConstraints());
+    startCapture();
+}
+
+async function startCapture() {
+    try {
+        const displayMediaOptions = {
+            video: {
+                cursor: "motion"
+            }
+        };
+        videoElem.srcObject = await navigator.mediaDevices.getDisplayMedia(displayMediaOptions);
+        audioElem.srcObject = await navigator.mediaDevices.getUserMedia({
+            audio: {
+              echoCancellation: true,
+              noiseSuppression: true,
+              sampleRate: 44100
+            },
+            video: false,
+        });
+        videoElem.srcObject.getVideoTracks().forEach((track) => {
+            track.contentHint = 'text';
+        });
+    } catch(err) {
+        console.error("Error: " + err);
+    }
+}
+
+function stopCapture (evt) {
+    let videoTracks = videoElem.srcObject.getTracks();
+    console.log(audioElem.srcObject)
+    let audioTracks = audioElem.srcObject.getTracks();
+  
+    videoTracks.forEach(track => track.stop());
+    videoElem.srcObject = null;
+
+    audioTracks.forEach(track => track.stop());
+    audioElem.srcObject = null;
 }
